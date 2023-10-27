935c964fa519c808aefbca7eeb422a79761b3525
stu8pi <146557595+stu8pi@users.noreply.github.com>
2023-10-27 08:23:46 +0000

    я крутой

--- task4pro.py
+++ task4pro.py
@@ -1,4 +1,10 @@
 num = int(input())
-for i in range(1, num+1):
+for i in range(2, num+1):
     if num % i == 0:
-        print(i)
+        flag = True
+        for k in range(2, i):
+            if i % k == 0:
+                flag = False
+                break
+        if flag == True:
+            print(i)  
